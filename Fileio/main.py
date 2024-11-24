a6b44bb0c161cb0b2309c45b62efd7952ec2d5a4
Ksu1106 <skywolfmarkova11@gmail.com>
2024-11-24 17:10:22 +0300

    Добавлено обработка исключений

--- Fileio/main.py
+++ Fileio/main.py
@@ -1,17 +1,22 @@
 from tkinter import *
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from tkinter import ttk
 import requests
 
 def upliad():
-    filepath = fd.askopenfilename()
-    if filepath:
-        files={'file': open(filepath, 'rb')}
-        response = requests.post('https://www.file.io',files = files)
-        if response.status_code==200:
-             link = response.json()['link']
-            entry.insert(0,link)
-
+    try:
+        filepath = fd.askopenfilename()
+        if filepath:
+            with open(filepath, 'rb') as f:
+                files={'file': f}
+                response = requests.post('https://file.io',files = files)
+                response.raise_for_status()
+                link = response.json()['link']
+                entry.delete(0,END)
+                entry.insert(0,link)
+    except Exception as e:
+        mb.showerror('Ошибка',f'Произошла ошибка: {e}')
 
 window = Tk()
 window.title()
@@ -20,7 +25,7 @@ window.geometry('400x200')
 buton = ttk.Button(text='Загрузить файл',command=upliad)
 buton.pack()
 
-entry= ttk.Entry
+entry= ttk.Entry()
 entry.pack()
 
 window.mainloop()
